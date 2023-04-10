83981e7ae17730732906495223b1c7a5299f0424
Nikudanngo <satoru_rutosa@icloud.com>
2023-04-10 16:03:18 +0900

    deployコマンド追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
+  // basePath: "_next/",
+  // assetPrefix: "https://www.oit.ac.jp/rd/labs/kobayashi-lab/~endo/",
+  // assetPrefix: "",
+  assetPrefix: isProd ? "https://www.oit.ac.jp/rd/labs/kobayashi-lab/~endo/" : "",
 }
 
 module.exports = nextConfig
