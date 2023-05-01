10c784b73dee385a9437ccd3a1c5036293aab46d
Nikudanngo <satoru_rutosa@icloud.com>
2023-05-02 04:54:52 +0900

    iframe埋め込み

--- src/components/MyRoom.tsx
+++ src/components/MyRoom.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import { ChaoticOrbit } from "@uiball/loaders";
 
 import { MyRoomModel } from "./MyRoomModel";
@@ -10,19 +9,6 @@ export const MyRoom = () => {
   return (
     <>
       <Canvas>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <OrbitControls
-          target={[0, 2.5, 0]} // カメラの中心を上に移動
-          enableDamping={true} // 慣性の有効化
-          dampingFactor={0.05} // 慣性の減衰率
-          enablePan={false} // パン操作の無効化
-          minDistance={1} // ズームインの最小距離
-          maxDistance={10} // ズームアウトの最大距離
-          maxPolarAngle={Math.PI / 2} // 上方向への回転の制限
-          minAzimuthAngle={-Math.PI / 4} //　左右の回転の制限
-          maxAzimuthAngle={Math.PI / 4}
-        />
         <MyRoomModel
           onLoaded={() => {
             setIsLoaded(true);
